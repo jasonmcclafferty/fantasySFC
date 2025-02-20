67868c8daab33a35adde398dd4bbe3185bcdce42
emccjas <jason.mc.clafferty@ericsson.com>
2025-02-20 14:36:00 +0000

    Commit Zero

---
+++ entrypoint.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Dia duit, a shaol!")
+
+}
---
+++ scraper.go
@@ -0,0 +1,12 @@
+package main
+
+// Alias fmt as fmt
+
+// Overall goal: Pull county GAA player data from
+// https://www.finalwhistle.ie/gaelic/
+// and populate the structs in structs.go
+
+// Initial goal - scrape a single county game data from the most recent national league fixture.
+func scrape() {
+
+}
